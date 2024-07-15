e1b75c791d4931f33fb8278c88829d3b2753a974
LucasdMarquez <lucasdariomarquez1992@gmail.com>
2024-07-15 04:17:11 -0300

    PreEntrega1-Marquez

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+import carrito from "../assets/carrito.png";
+
+export const CartWidget = () => (
+    <>
+    
+    <img src={carrito} height={30} />
+    <span> 6</span> 
+    </>
+);
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,4 @@
+import Container from 'react-bootstrap/Container';
+
+export const ItemListContainer = ({ greeting}) => (
+<Container className='mt-3'> <h1>{greeting}</h1></Container>); 
